4cef44001c87a7f461edbb9420702e9543f250ed
liudng <liudng@gmail.com>
2015-08-18 10:51:23 +0200

    cd

--- coding.go
+++ coding.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/zhgo/console"
 	"log"
+	"os"
+	"path/filepath"
 	"text/template"
 )
 
@@ -14,57 +16,80 @@ func main() {
 	tBiz := template.Must(template.New("biz").Funcs(funcMap).Parse(tplBiz))
 	tCtr := template.Must(template.New("ctr").Funcs(funcMap).Parse(tplCtr))
 	tEnt := template.Must(template.New("ent").Funcs(funcMap).Parse(tplEnt))
-	tIom := template.Must(template.New("iom").Funcs(funcMap).Parse(tplIom))
-	tMod := template.Must(template.New("mod").Funcs(funcMap).Parse(tplMod))
-	tTab := template.Must(template.New("tab").Funcs(funcMap).Parse(tplTab))
+	tModHeader := template.Must(template.New("modHeader").Funcs(funcMap).Parse(tplModHeader))
+	tMod1 := template.Must(template.New("mod1").Funcs(funcMap).Parse(tplMod1))
+	tMod2 := template.Must(template.New("mod2").Funcs(funcMap).Parse(tplMod2))
+	tMod3 := template.Must(template.New("mod3").Funcs(funcMap).Parse(tplMod3))
 
 	tAdd := template.Must(template.New("add").Funcs(funcMap).Parse(tplAdd))
 	tBrowse := template.Must(template.New("browse").Funcs(funcMap).Parse(tplBrowse))
 	tDetail := template.Must(template.New("detail").Funcs(funcMap).Parse(tplDetail))
 	tEdit := template.Must(template.New("edit").Funcs(funcMap).Parse(tplEdit))
 
+	mods := make(map[string][4]string)
+
 	for _, table := range tables {
 		data := allColumns(table)
 
 		// biz
-		p := console.WorkingDir + "/backend/biz/" + data.Const["table"] + ".go"
-		parseDft(data, p, tBiz)
+		p := console.WorkingDir + "/backend/biz/" + data.Const["module"] + ".go"
+		parseMdl(p, data, tBiz, "biz", tplBizHeader)
 
 		// ctr
 		p = console.WorkingDir + "/backend/ctr/" + data.Const["table"] + ".go"
-		parseDft(data, p, tCtr)
+		parseDft(p, data, tCtr)
 
 		// ent
 		p = console.WorkingDir + "/backend/ent/" + data.Const["module"] + ".go"
-		parseMdl("ent", data, p, tEnt, tplEntHeader)
-
-		// iom
-		p = console.WorkingDir + "/backend/iom/" + data.Const["module"] + ".go"
-		parseMdl("iom", data, p, tIom, tplIomHeader)
+		parseMdl(p, data, tEnt, "ent", tplEntHeader)
 
 		// mod
-		p = console.WorkingDir + "/backend/mod/" + data.Const["table"] + ".go"
-		parseDft(data, p, tMod)
-
-		// tab
-		p = console.WorkingDir + "/backend/tab/" + data.Const["module"] + ".go"
-		parseMdl("tab", data, p, tTab, tplTabHeader)
+		m := mods[data.Const["module"]]
+		m[0] = parseStr(data, tModHeader)
+		m[1] += parseStr(data, tMod1)
+		m[2] += parseStr(data, tMod2)
+		m[3] += parseStr(data, tMod3)
+		mods[data.Const["module"]] = m
 
 		// add
 		p = console.WorkingDir + "/frontend/" + data.Const["module"] + "/" + data.Const["entity"] + "_add.jsx"
-		parseDft(data, p, tAdd)
+		parseDft(p, data, tAdd)
 
 		// browse
 		p = console.WorkingDir + "/frontend/" + data.Const["module"] + "/" + data.Const["entity"] + "_browse.jsx"
-		parseDft(data, p, tBrowse)
+		parseDft(p, data, tBrowse)
 
 		// detail
 		p = console.WorkingDir + "/frontend/" + data.Const["module"] + "/" + data.Const["entity"] + "_detail.jsx"
-		parseDft(data, p, tDetail)
+		parseDft(p, data, tDetail)
 
 		// edit
 		p = console.WorkingDir + "/frontend/" + data.Const["module"] + "/" + data.Const["entity"] + "_edit.jsx"
-		parseDft(data, p, tEdit)
+		parseDft(p, data, tEdit)
+	}
+
+	// mod
+	for k, v := range mods {
+		p := console.WorkingDir + "/backend/mod/" + k + ".go"
+
+		err := os.MkdirAll(filepath.Dir(p), os.ModeDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		f, err := os.Create(p)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, str := range v {
+			_, err := f.WriteString(str)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		f.Close()
 	}
 
 	closeFiles()
